27ece566e396232410fa1b2858c2a79ee4e95a42
Molly Kent <kent.molly@gmail.com>
2015-11-05 16:10:42 -0800

    Fix tests so they pass

--- test/router_test.js
+++ test/router_test.js
@@ -6,8 +6,47 @@ describe('sea d45 router', function() {
     this.router = new Router();
   });
 
+  it('should be a router', function() {
+    expect(this.router.routes).to.have.property('GET');
+    expect(this.router.routes).to.have.property('POST');
+    expect(this.router.routes).to.have.property('PUT');
+    expect(this.router.routes).to.have.property('PATCH');
+    expect(this.router.routes).to.have.property('DELETE');
+  });
+
+  it('should 404', function() {
+    var cbCalled = {
+      writeHead: false,
+      write: false,
+      end: false
+    };
+    var req = {
+      url: 'doesnotexist',
+      method: 'GET'
+    };
+    var res = {
+      writeHead: function(status, headers) {
+        cbCalled.writeHead = true;
+        expect(status).to.eql(404);
+        expect(headers).to.eql({'Content-Type' : 'text/plain'});
+      },
+      write: function(text) {
+        cbCalled.write = true;
+        expect(text).to.eql('not found');
+      },
+      end: function() {
+        cbCalled.end = true;
+      }
+    };
+    this.router.route(req, res);
+    expect(cbCalled.writeHead).to.eql(true);
+    expect(cbCalled.write).to.eql(true);
+    expect(cbCalled.end).to.eql(true);
+  });
+
   describe('rest handling', function() {
-    before(function() {
+    beforeEach(function() {
+      this.router = new Router();
       this.req = {
         method: '',
         url: 'testurl'
@@ -16,51 +55,16 @@ describe('sea d45 router', function() {
         test: 'some test'
       };
     });
-  
-    it('should be a router', function() {
-      expect(this.router.routes).to.have.property('GET');
-      expect(this.router.routes).to.have.property('POST');
-      expect(this.router.routes).to.have.property('PUT');
-      expect(this.router.routes).to.have.property('PATCH');
-      expect(this.router.routes).to.have.property('DELETE');
-    });
-
-    it('should 404', function() {
-      var cbCalled = {
-        writeHead: false,
-        write: false,
-        end: false
-      };
-      this.req.method = 'GET';
-      var res = {
-        writeHead: function(status, headers) {
-          cbCalled.writeHead = true;
-          expect(status).to.eql(404);
-          expect(headers).to.eql({'Content-Type' : 'text/plain'});
-        },
-        write: function(text) {
-          cbCalled.write = true;
-          expect(text).to.eql('not found');
-        },
-        end: function() {
-          cbCalled.end = true;
-        }
-      };
-      this.router.route(this.req, res);
-      expect(cbCalled.writeHead).to.eql(true);
-      expect(cbCalled.write).to.eql(true);
-      expect(cbCalled.end).to.eql(true);
-    });
 
     it('should be able to setup a get route', function() {
       this.req.method = 'GET';
       var cbCalled = false;
 
-      this.router.get('testurl', function(testreq, testres) {
+      this.router.get('testurl', function(testReq, testRes) {
         cbCalled = true;
-        expect(testreq).to.eql(this.req);
-        expect(testres).to.eql(this.res);
-      }).bind(this);
+        expect(testReq).to.eql(this.req);
+        expect(testRes).to.eql(this.res);
+      }.bind(this));
 
       this.router.route(this.req, this.res);
       expect(cbCalled).to.eql(true);
@@ -74,7 +78,7 @@ describe('sea d45 router', function() {
         cbCalled = true;
         expect(testreq).to.eql(this.req);
         expect(testres).to.eql(this.res);
-      }).bind(this);
+      }.bind(this));
 
       this.router.route(this.req, this.res);
       expect(cbCalled).to.eql(true);
